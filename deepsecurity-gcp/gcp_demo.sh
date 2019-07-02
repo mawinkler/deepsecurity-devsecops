44ce6fdee59e37c8373b3e14b9c7dc11143bf8bd
mawinkler <winkler.info@icloud.com>
2019-07-02 16:27:54 +0200

    create, deploy, terminate

--- deepsecurity-gcp/gcp_demo.sh
+++ deepsecurity-gcp/gcp_demo.sh
@@ -1,3 +1,16 @@
 #/bin/bash
+
+echo Running Playbook gcp_create_instances.yml
 ansible-playbook --vault-password-file ../../.vault-pass.txt -i gcp_inventory.gcp.yml gcp_create_instances.yml
-ansible-playbook --vault-password-file ../../.vault-pass.txt -i gcp_inventory.gcp.yml gcp_reset_windows_password.yml
+
+echo Running Playbook gcp_reset_windows_passwords.yml
+ansible-playbook --vault-password-file ../../.vault-pass.txt -i gcp_inventory.gcp.yml gcp_reset_windows_passwords.yml
+
+echo Running Playbook gcp_dsa_facter_install.yml
+ansible-playbook --vault-password-file ../../.vault-pass.txt -i gcp_inventory.gcp.yml gcp_dsa_facter_install.yml
+
+echo Sleeping for 2 minutes
+sleep 120
+
+echo Running Playbook gcp_terminate.yml
+ansible-playbook --vault-password-file ../../.vault-pass.txt -i gcp_inventory.gcp.yml gcp_terminate.yml
