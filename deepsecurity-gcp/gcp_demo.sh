6a97853b974b3a655de3a7638659b0b8146947d1
mawinkler <winkler.info@icloud.com>
2019-07-09 17:27:09 +0200

    initial version

--- deepsecurity-gcp/gcp_demo.sh
+++ deepsecurity-gcp/gcp_demo.sh
@@ -9,8 +9,8 @@ ansible-playbook --vault-password-file ../../.vault-pass.txt -i gcp_inventory.gc
 echo Running Playbook gcp_dsa_facter_install.yml
 ansible-playbook --vault-password-file ../../.vault-pass.txt -i gcp_inventory.gcp.yml gcp_dsa_facter_install.yml
 
-echo Sleeping for 2 minutes
-sleep 120
+echo Sleeping for 5 minutes
+sleep 300
 
 echo Running Playbook gcp_terminate.yml
 ansible-playbook --vault-password-file ../../.vault-pass.txt -i gcp_inventory.gcp.yml gcp_terminate.yml
