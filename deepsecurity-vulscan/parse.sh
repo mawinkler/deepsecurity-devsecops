25a9a1147fcbfd110d2ebe25759b96a3c705d32b
Markus Winkler <winkler.markus@me.com>
2021-05-20 14:37:34 +0200

    removed

--- deepsecurity-vulscan/parse.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# call with ./report_parser.sh <report file>
-cat $1 | egrep -o 'CVE-[0-9]+-[0-9]+' | egrep -o 'CVE-[0-9]+-[0-9]+' | sort | uniq | awk 'BEGIN{}{printf("%s ", $0)}END{}'
